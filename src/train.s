17e548b6d935941aa25973354d9019ddf258989a
r4m-t1n <im.r4mt1n@gmail.com>
2025-07-20 19:40:28 +0330

    fixed with use absolute adr & saving fp and lr & use callee saved regs

--- src/train.s
+++ src/train.s
@@ -17,77 +17,111 @@ zero_float:         .float 0.0
 
 .text
 main:
-    adr x9, learning_rate         // address of learning_rate to x9
-    ldr s9, [x9]                  // number of learning_rate to s9
+    ldr x9, =learning_rate         // address of learning_rate to x9
+    ldr s23, [x9]                  // number of learning_rate to s9
 
-    adr x10, x                    // address of array x
-    adr x11, y                    // address of array y
+    ldr x10, =x                    // address of array x
+    ldr x11, =y                    // address of array y
 
-    adr x12, weight_              // address of weight_ buffer to x12
-    ldr s12, [x12]                // number of weight_ to s12
-    adr x13, bias_                // address of bias_ buffer to x13
-    ldr s13, [x13]                // number of bias_ to s13
+    ldr x12, =weight_              // address of weight_ buffer to x12
+    ldr s16, [x12]                 // number of weight_ to s0
+    ldr x13, =bias_                // address of bias_ buffer to x13
+    ldr s17, [x13]                 // number of bias_ to s1
 
-    adr x18, sample_size          // address of sample_size to s18
-    ldr s19, [x18]                // number of sample_size to s18
+    ldr x18, =sample_size          // address of sample_size to s18
+    ldr s24, [x18]                 // number of sample_size to s18
 
-    adr x4, epochs                // address of epochs buffer to x4
-    ldr w4, [x4]                  // number of epochs to w4
+    ldr x4, =epochs                // address of epochs buffer to x4
+    ldr w19, [x4]                  // number of epochs to w4
 
-    mov w1, 0                     // index of train_loop from 0 to epochs
+    mov w20, 0                     // index of train_loop from 0 to epochs
     b train_loop
 
 train_loop:
-    adr x0, zero_float            // address of zero_float to s0
-    ldr s25, [x0]                 // number of zero_flat to s25
+    adr x0, zero_float             // address of zero_float to s0
+    ldr s25, [x0]                  // number of zero_flat to s25
 
-    fmov s20, s25                 // sum of derivatives of weight
-    fmov s21, s25                 // sum of derivatives of bias
-    fmov s22, s25                 // total loss
+    fmov s18, s25                  // sum of derivatives of weight
+    fmov s19, s25                  // sum of derivatives of bias
+    fmov s20, s25                  // total loss
 
-    mov w5, 0                     // index of forward_loop from 0 to sample
-    mov x5, 0
+    mov x0, 0                      // index of forward_loop from 0 to sample
 
     bl forward_loop
 
-    fdiv s20, s20, s19
-    fdiv s21, s21, s19
+    fdiv s18, s18, s24
+    fdiv s19, s19, s24
+
     bl optimize
 
-    adr x0, epoch_string          // address of epoch_string
-    mov w1, w1                    // not necessary but to know that the second argument is for epoch number
-    fmov s0, s12                  // weight
-    fmov s1, s13                  // bias
-    fdiv s2, s22, s19             // loss
+    // save registers:
+    stp x29, x30, [sp, -112]!      // save fp and lr, allocate 112 bytes for registers
+    mov x29, sp                    // update fp
+
+    stp x9, x10, [sp, 16]          // x9 (learning_rate address), x10 (x array address)
+    stp x11, x12, [sp, 32]         // x11 (y array address), x12 (weight_ address)
+    stp x13, x18, [sp, 48]         // x13 (bias_ address), x18 (sample_size address)
+    str w20, [sp, 64]              // w20 (epoch)
+    str w19, [sp, 68]              // w19 (epochs)
+
+    stp s16, s17, [sp, 72]         // s16 (weight), s17 (bias)
+    stp s18, s19, [sp, 88]         // s18 (sum of dw), s19 (sum of db)
+    stp s20, s23, [sp, 104]        // s20 (total loss), s23 (learning_rate)
+    str s24, [sp, 120]             // s24 (sample_size)
+
+    adr x0, epoch_string           // address of epoch_string
+    mov w1, w20                    // not necessary but to know that the second argument is for epoch number
+    fmov s0, s16                   // weight
+    fmov s1, s17                   // bias
+    fdiv s2, s20, s24              // loss
     bl printf
 
-    add w1, w1, 1
-    cmp w1, w4
+    // restore registers:
+    ldr s24, [sp, 120]
+    ldp s20, s23, [sp, 104]
+    ldp s18, s19, [sp, 88]
+    ldp s16, s17, [sp, 72]
+
+    ldr w19, [sp, 68]
+    ldr w20, [sp, 64]
+    ldp x13, x18, [sp, 48]
+    ldp x11, x12, [sp, 32]
+    ldp x9, x10, [sp, 16]
+
+    ldp x29, x30, [sp], #112       // restore fp and lr and adjust sp
+
+    add w20, w20, 1
+    cmp w20, w19
     blt train_loop
 
     b exit
 
 
 forward_loop:
-    lsl x1, x5, 2
-    ldr s10, [x10, x1]            // s10 = x[i]
-    ldr s11, [x11, x1]            // s11 = y[i]
+    stp x29, x30, [sp, -16]!       // save frame pointer and link register
+    mov x29, sp
+
+    lsl x1, x0, 2
+    ldr s3, [x10, x1]              // s3 = x[i]
+    ldr s4, [x11, x1]              // s4 = y[i]
+
+
+    bl linear_model                // goes to model.s and calculates the y_hat in s14
+
+    bl mean_squared_error          // goes to loss.s and calculates the loss in s5
 
     bl back_propagation
-    fadd s20, s20, s16            // with s16 being the derivative of weight, calculated from optimizer.s file
-    fadd s21, s21, s17            // with s17 being the derivative of bias, calculated from optimizer.s file
 
-    bl linear_model               // goes to model.s and calculates the y_hat in s14
+    fadd s18, s18, s6              // with s6 being the derivative of weight, calculated from optimizer.s file
+    fadd s19, s19, s7              // with s7 being the derivative of bias, calculated from optimizer.s file
+    fadd s20, s20, s5              // adds the calculated loss to the total loss
 
-    bl mean_squared_error         // goes to loss.s and calculates the loss in s15
-
-    fadd s22, s22, s15            // adds the calculated loss to the total loss
-
-    add w5, w5, 1
-    add x5, x5, 1
-    scvtf s24, w5
-    fcmp s24, s19                 // s19 = sample_size
+    add x0, x0, 1
+    scvtf s25, x0
+    fcmp s25, s24                  // s19 = sample_size
     blt forward_loop
+
+    ldp x29, x30, [sp], #16        // restore fp and lr
 
     ret
 
