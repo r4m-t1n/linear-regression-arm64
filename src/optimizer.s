473c168351e8ab5ce75a657be91a58abd12ac23b
r4m-t1n <im.r4mt1n@gmail.com>
2025-07-21 21:06:20 +0330

    change registers

--- src/optimizer.s
+++ src/optimizer.s
@@ -3,7 +3,7 @@
 
 .text
 back_propagation:
-    fmov s25, -2.0              // -2.0 to s7
+    fmov s25, -2.0              // -2.0 to s25
     fsub s8, s4, s2             // error = y[i] - y_hat
 
     fmul s6, s8, s3             // dw = -2 * error * x[i]
@@ -17,11 +17,11 @@ optimize:
     stp x29, x30, [sp, -16]!    // save frame pointer and link register
     mov x29, sp
 
-    fmul s25, s23, s18
-    fmul s16, s16, s25
+    fmul s26, s23, s18
+    fmul s27, s23, s19
 
-    fsub s25, s23, s19          // w - (learning_rate * dw)
-    fsub s17, s17, s25          // b - (learning_rate * db)
+    fsub s16, s16, s26          // w - (learning_rate * dw)
+    fsub s17, s17, s27          // b - (learning_rate * db)
 
     ldp x29, x30, [sp], #16     // restore fp and lr
 
